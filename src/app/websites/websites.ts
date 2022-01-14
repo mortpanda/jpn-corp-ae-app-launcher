ec1cdf2245291948dd27f26f6d207ff4ebb85ff4
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-01-14 23:25:00 +0900

    main
    main

--- src/app/websites/websites.ts
+++ src/app/websites/websites.ts
@@ -6,9 +6,12 @@ export interface Websites {
 
 export const OtherWebsite = [
   {
-    name: 'Okta',
-    url: 'https://okta.okta.com/',
-
+    name: 'Japan Corporate AE Org Dashboard',
+    url: 'https://mortpanda.github.io/jpn-corp-ae-siw',
+  },
+  {
+    name: 'Portal Application (OIDC SSO)',
+    url: 'https://mortpanda.github.io/jpn-corp-ae-portal-app',
   },
 ]
 
@@ -17,4 +20,7 @@ export const OtherWebsite = [
 //https://mortpanda.github.io/jpn-corp-ae-portal-app/home
 //https://mortpanda.github.io/jpn-corp-ae-portal-profile/
 //https://mortpanda.github.io/jpn-corp-ae-portal-calc/
-//https://mortpanda.github.io/jpn-corp-ae-siw/start
+
+
+
+//https://mortpanda.github.io/jpn-corp-ae-siw/
