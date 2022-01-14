227f75968e22e23e071e42c27f68a8c4049721b1
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-01-14 19:00:51 +0900

    main
    main

--- src/app/websites/websites.ts
+++ src/app/websites/websites.ts
@@ -11,3 +11,9 @@ export const OtherWebsite = [
 
   },
 ]
+
+
+//https://mortpanda.github.io/jpn-corp-ae-portal-app/home
+//https://mortpanda.github.io/jpn-corp-ae-portal-app/home
+//https://mortpanda.github.io/jpn-corp-ae-portal-profile/
+//https://mortpanda.github.io/jpn-corp-ae-portal-calc/
