6215e08a3257bddd88e04c317831e9f2e906e9dd
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-01-18 15:32:38 +0900

    main
    main

--- src/app/websites/websites.ts
+++ src/app/websites/websites.ts
@@ -6,14 +6,24 @@ export interface Websites {
 
 export const OtherWebsite = [
   {
-    name: 'Japan Corporate AE Org Dashboard',
+    name: 'Jpn Corp AE Org End User Dashboard',
     url: 'https://mortpanda.github.io/jpn-corp-ae-siw',
+  },
+  {
+    name: 'Jpn Corp AE Org Admin Dashboard',
+    url: 'https://jpn-corp-ae-admin.oktapreview.com/admin/dashboard',
+  },
+  {
+    name: 'CIAM Membership Site',
+    url: 'https://mortpanda.github.io/jpn-corp-ae-ciam-member-site-1/',
   },
   {
     name: 'THE PORTAL (OIDC SSO)',
     url: 'https://mortpanda.github.io/jpn-corp-ae-portal-app',
   },
 ]
+
+
 
 
 
